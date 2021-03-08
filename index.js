e864f975d62b811651874fd8bf9908e77f41e036
simokh <m.simokhouili@gmail.com>
2021-03-07 22:32:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+
+
+let main = document.querySelector('#main')
+main.remove()
+
+let newHeader = document.createElement('h1');
+newHeader.id = "victory";
+newHeader.innerHTML = "Simo is the champion"
+
+
+
